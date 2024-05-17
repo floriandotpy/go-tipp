e862fc905ac7ec9c6ec2067ffd4ac6d4c152a502
Florian <florian@casualcoding.com>
2024-05-17 15:21:37 +0200

    Start loading game schedule from json file

--- tipp.go
+++ tipp.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"time"
+	"io/ioutil"
+	"os"
+	// "time"
 )
 
 const TEAM_DE = "Deutschland"
@@ -30,14 +33,19 @@ const TEAM_PL = "Polen"
 const TEAM_UA = "Ukraine"
 const TEAM_GR = "Griechenland"
 
+type Games struct {
+	Games []Game `json:"games"`
+}
+
 type Game struct {
 	TeamA     string
 	TeamB     string
-	StartTime time.Time
+	StartTime string //time.Time
+	Title     string
 
 	// goal numbers
-	ResultA *int
-	ResultB *int
+	// ResultA *int
+	// ResultB *int
 }
 
 type Tipp struct {
@@ -46,10 +54,31 @@ type Tipp struct {
 }
 
 func main() {
+	// load games
+	jsonFile, err := os.Open("games.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Opened games.json")
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var games Games
+	json.Unmarshal(byteValue, &games)
+
+	for i := 0; i < len(games.Games); i++ {
+		fmt.Println("StartTime: " + games.Games[i].StartTime)
+		fmt.Println("TeamA: " + games.Games[i].TeamA)
+		fmt.Println("TeamB: " + games.Games[i].TeamB)
+		fmt.Println("")
+	}
+
 	// load the location you want to display the time in
-	loc, _ := time.LoadLocation("Europe/Berlin")
-	g1 := &Game{TeamA: TEAM_DE, TeamB: TEAM_DK,
-		StartTime: time.Date(2024, 6, 14, 21, 0, 0, 0, loc),
-		ResultA:   nil, ResultB: nil}
-	fmt.Println(g1)
+	// loc, _ := time.LoadLocation("Europe/Berlin")
+	// g1 := &Game{TeamA: TEAM_DE, TeamB: TEAM_DK,
+	// 	StartTime: time.Date(2024, 6, 14, 21, 0, 0, 0, loc),
+	// 	ResultA:   nil, ResultB: nil}
+	// fmt.Println(g1)
 }
