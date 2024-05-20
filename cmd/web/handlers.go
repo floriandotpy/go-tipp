ed89bba70069894c2b09c4d77d663eddde4ccc02
Florian <florian@casualcoding.com>
2024-05-20 20:21:31 +0200

    Add html templating

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -60,11 +62,8 @@ func loadGames() (*Games, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	fmt.Println("Opened games.json")
-
 	defer jsonFile.Close()
-
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 	var games Games
 	json.Unmarshal(byteValue, &games)
@@ -73,19 +72,43 @@ func loadGames() (*Games, error) {
 }
 
 func gamesHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "Games:")
 
 	games, err := loadGames()
 	if err != nil {
 		fmt.Println(err)
 	}
+	// for now just log to console
+	fmt.Println(games)
 
-	for i := 0; i < len(games.Games); i++ {
-		fmt.Fprintln(w, "StartTime: "+games.Games[i].StartTime)
-		fmt.Fprintln(w, "TeamA: "+games.Games[i].TeamA)
-		fmt.Fprintln(w, "TeamB: "+games.Games[i].TeamB)
-		fmt.Fprintln(w, "")
+	// for i := 0; i < len(games.Games); i++ {
+	// 	fmt.Fprintln(w, "StartTime: "+games.Games[i].StartTime)
+	// 	fmt.Fprintln(w, "TeamA: "+games.Games[i].TeamA)
+	// 	fmt.Fprintln(w, "TeamB: "+games.Games[i].TeamB)
+	// 	fmt.Fprintln(w, "")
+	// }
+
+	// templates
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/partials/nav.html",
+		"./ui/html/pages/home.html",
 	}
+
+	// load template
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// execute template
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
 }
 
 // view a single submitted tipp instance
