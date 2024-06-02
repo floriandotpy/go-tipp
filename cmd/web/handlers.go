96893e93491dc64a64eff0d3b2f6a4a61e7b0910
Florian <florian@casualcoding.com>
2024-06-02 16:24:32 +0200

    Fix: add missing quote

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -259,7 +259,7 @@ func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
 type userLoginForm struct {
 	Email               string `form:"email"`
 	Password            string `form:"password"`
-	validator.Validator `form:"-`
+	validator.Validator `form:"-"`
 }
 
 func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
